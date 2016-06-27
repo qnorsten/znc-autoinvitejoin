d0b61291c5ac7e85c049836f96ce88e141251b1f
Christophe Beauval <Cizzle@users.noreply.github.com>
2016-06-27 12:05:43 +0200

    Add module

---
+++ autoinvitejoin.cpp
@@ -0,0 +1,45 @@
+#include <znc/main.h>
+#include <znc/Modules.h>
+#include <znc/Chan.h>
+#include <znc/IRCNetwork.h>
+
+/*
+ * AutoInviteJoin module
+ * Author: Cizzle
+ * Description: Joins saved channels upon invite, even when the user is not connected.
+ * Version: 20160621
+ */
+
+using std::set;
+
+class CAutoInviteJoin : public CModule {
+public:
+
+	MODCONSTRUCTOR(CAutoInviteJoin) {}
+
+	virtual ~CAutoInviteJoin() {}
+
+	virtual EModRet OnInvite(const CNick& Nick, const CString& sChan) override {
+		CIRCNetwork *pNetwork = GetNetwork();
+		if (pNetwork) {
+			/* Only autojoin channels in our config */
+			CChan *invChan = pNetwork->FindChan(sChan);
+			if (invChan) {
+				set<CChan*> sChans;
+				sChans.insert(invChan);
+				pNetwork->JoinChans(sChans);
+			}
+		}
+
+		return CONTINUE;
+	}
+};
+
+template <>
+void TModInfo<CAutoInviteJoin>(CModInfo& Info) {
+	Info.SetWikiPage("autoinvitejoin");
+	Info.AddType(CModInfo::UserModule);
+	Info.AddType(CModInfo::GlobalModule);
+}
+
+NETWORKMODULEDEFS(CAutoInviteJoin, "Auto joins saved channels on invite even when the user is not connected.")
